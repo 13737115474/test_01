c65b94385df10a060cb8ddc602e77678a4ac4557
黄昭腾 <502467868@qq.com>
2022-06-24 14:03:24 +0800

    所有的学习代码

---
+++ 评论列表案例/cmt.js
@@ -0,0 +1,37 @@
+function getCommentList(params) {
+    $.ajax({
+        method: "GET",
+        url: "http://www.liulongbin.top:3006/api/cmtlist",
+        data: {},
+        success: function (res) {
+            if (res.status != 200) {
+                return alert("获取评论列表失败!");
+            }
+            var rows = [];
+            $.each(res.data, function (i, item) {
+                var str = '<li class="list-group-item"><span class="badge" style="background-color: #f0ad4e;">评论人：' + item.username + '</span><span class="badge" style="background-color: #5bc0de;">评论时间:' + item.time + '</span>' + item.content + '</li>'
+                rows.push(str);
+            })
+            //join()中的参数分割数组中元素,这里是空格
+            $("#cmt-list").empty().append(rows.join(""));
+        }
+    })
+}
+
+getCommentList();
+
+$(function () {
+    $("#formAddCmt").submit(function (e) {
+        e.preventDefault();
+        //serialize()获取表单中所有的数据，表单元素必须要有name属性
+        var data = $(this).serialize();
+        $.post("http://www.liulongbin.top:3006/api/addcmt", data, function (res) {
+            if (res.status != 201) {
+                return alert("发表评论失败!");
+            }
+            getCommentList();
+            //将JQuery对象转换为Dom对象，使用Dom的方法reset()
+            $("#formAddCmt")[0].reset();
+        })
+    })
+})
